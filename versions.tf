4f3cca55886fa7008c5c73343551b86b3f638607
dmitryuchuvatov <119931089+dmitryuchuvatov@users.noreply.github.com>
2023-01-10 10:15:40 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "duchuvatov-test"
 
     workspaces {
       name = "sentinel-example"
